04799ae38f778359aa840bcee4549e83375b55c6
francispinheiro <efransys@gmail.com>
2022-09-08 16:49:40 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,24 @@
 import { useState } from 'react'
+import Header from './components/Header/indext';
+
 import NotificationButton from "./components/NotificationButton";
 
+import '../src/index.css';
+import SalesCard from './components/SalesCard';
 
 function App() {
 
   return (
     <>
-      <h1>oLa que tal!</h1>
-      <NotificationButton />
+      <Header/>
+      
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard/>
+          </div>
+        </section>
+      </main>    
     </>
   )
 }
